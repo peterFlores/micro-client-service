397769e2a76cb9012d5a2ebc0282fd5febad1e1b
Pedro  Flores <pedro.flores@bowpi.com>
2021-09-16 15:08:10 -0700

    cors config

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix("api/v1")
+  app.enableCors({
+    origin: 'http://localhost:4200'
+    });
   await app.listen(3001);
 }
 bootstrap();
